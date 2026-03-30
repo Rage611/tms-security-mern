b63c41b449f3cb0de346ac60502df49aed81f022
Rishabh <ajaytmsgroup78@gmail.com>
2026-03-30 17:19:20 +0530

    Fixed a bug in Cloudinary

--- frontend/src/components/NavbarContents/Career/Career.jsx
+++ frontend/src/components/NavbarContents/Career/Career.jsx
@@ -87,7 +87,7 @@ const Career = () => {
 
       await emailjs.send(
         import.meta.env.VITE_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        import.meta.env.VITE_EMAILJS_CAREER_TEMPLATE_ID,
         templateParams,
         import.meta.env.VITE_EMAILJS_PUBLIC_KEY
       );
