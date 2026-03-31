896c6eb04df38bea9cc03656c619e6178e0c3dec
Rishabh <ajaytmsgroup78@gmail.com>
2026-03-31 14:30:48 +0530

    Refactor: Implement shared global CSS layout for all service pages

--- frontend/src/components/NavbarContents/Services/ManPowerServices/ManPowerContent/PSO/PSO.jsx
+++ frontend/src/components/NavbarContents/Services/ManPowerServices/ManPowerContent/PSO/PSO.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
+import '../../../SharedServiceLayout.css';
+
 // Pre-applied the deep nesting fix for your image paths!
 import serviceBanner from '../../../../../../assets/images/hero/hero-3.png'; 
 import psoImg from '../../../../../../assets/images/NavbarServices/pso.png'; 
