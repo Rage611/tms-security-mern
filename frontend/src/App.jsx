79cab3aca679e097c3b097da37064df19616dd23
Rishabh <ajaytmsgroup78@gmail.com>
2026-05-30 17:58:25 +0530

    Glitch Solved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { lazy, Suspense } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from '@vercel/speed-insights/react';
 
